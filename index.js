462be76eb352e57378f11aa6bc926f3ce4691006
DanielT-M <111027485+DanielT-M@users.noreply.github.com>
2023-10-15 17:49:49 -0600

    Add files via upload
    ruta de tareas agregada

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const app = express();
 app.use(express.json());
 
 const userRouter = require("./routers/usuario");
+const taskRouter = require("./routers/tarea");
 
 //Rutas
 app.use(userRouter);
+app.use(taskRouter);
+
 
 
 app.listen(8080, function () {
