a98382c4ef7aa70cb6b75b23775c27b57399cc80
DanielT-M <111027485+DanielT-M@users.noreply.github.com>
2023-10-15 16:05:28 -0600

    Add files via upload
    primera correccion

--- models/tarea.js
+++ models/tarea.js
@@ -2,25 +2,16 @@ const { DataTypes } = require("sequelize");
 const { sequelize } = require("./sequelize");
 
 module.exports = sequelize.define("tarea", {
-    uuid: {
+    /*uuid: {
         type: DataTypes.UUID,
         unique: true,
-    },
-
-    //nombre_tarea:{
-    //     type:DataTypes.STRING(100),
-    //     allowNull:false,
-    //},
+    },*/
 
     descripcion_mensaje: {
         type: DataTypes.STRING(100),
         allowNull: false,
     },
 
-    //materiales:{
-    //    type:DataTypes.STRING(100),
-    //    allowNull:false,
-    //},
 
     fecha_inicio: {
         type: DataTypes.DATE,
@@ -37,23 +28,4 @@ module.exports = sequelize.define("tarea", {
         defaultValue: 0,
     },
 
-    //id_estado:{
-    //    type:DataTypes.INTEGER,
-    //    defaultValue:0,
-    //},
-
-    //id_prioridad:{
-    //type:DataTypes.INTEGER,
-    //defaultValue:0,
-    //},
-
-    //horas:{
-    //    type:DataTypes.INTEGER,
-    //    defaultValue:0,
-    //},
-
-    //costo:{
-    //   type:DataTypes.INTEGER,
-    //    defaultValue:0,
-    //},
 });
