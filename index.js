7dcec83cac2d8a7d386029f4ac1a3768b7c5a5c2
DanielT-M <111027485+DanielT-M@users.noreply.github.com>
2023-10-16 02:04:22 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-const { initDatabase } = require("./db");
-initDatabase();
-
+require("dotenv").config();
+const { iniciarBaseDatos } = require("./db");
 const express = require("express");
-const app = express();
 
+const app = express();
+const PORT = process.env.SERVER_PORT || 8000; // Asegurando que SERVER_PORT esté configurado
+
+//Iniciar base de datos
+iniciarBaseDatos();
+
+// Middleware para recibir JSON
 app.use(express.json());
 
-const userRouter = require("./routers/usuario");
+
+const usuarioRouter = require("./routers/usuario");
+const loginRouter = require("./routers/auth");
 const taskRouter = require("./routers/tarea");
 
 //Rutas
-app.use(userRouter);
-app.use(taskRouter);
+app.use("/users", usuarioRouter);
+app.use("/login", loginRouter);
+app.use("/tasks", taskRouter);
 
 
+const validationError = require("./middlewares/validation-error");
+const unknownError = require("./middlewares/unknown-error");
+// Middleware de manejo de errores
+app.use(validationError);
+app.use(unknownError);
 
-app.listen(8080, function () {
-    console.log("> Escuchando puerto 8080");
+
+app.listen(PORT, () => {
+    console.log(`> Escuchando en el puerto ${PORT}`);
 });
