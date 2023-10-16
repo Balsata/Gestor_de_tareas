175a3ebe08ab2d1cc2128e05f9629bb8700652af
DanielT-M <111027485+DanielT-M@users.noreply.github.com>
2023-10-16 02:00:40 -0600

    API Tareas

--- routers/tarea.js
+++ routers/tarea.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createTask } = require("../controllers/tarea");
+const { crearTarea } = require("../controllers/tarea");
+const validator = require("../middlewares/validator");
+const { createTaskSchema } = require("../validations/tarea");
 
 
-router.post("/", createTask);
+router.post("/", validator.body(createTaskSchema), crearTarea);
 
 module.exports = router;
