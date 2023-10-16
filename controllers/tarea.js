9b258e769ae0ef0226cee06913ce83c25118a425
DanielT-M <111027485+DanielT-M@users.noreply.github.com>
2023-10-16 01:59:48 -0600

    API Tareas

--- controllers/tarea.js
+++ controllers/tarea.js
@@ -1,8 +1,9 @@
 const { insert } = require("../services/tarea.js");
 
 
-exports.createUser = async function (request, response) {
-    const { tarea, descripcion_mensaje, apellido_materno, afecha_inicio, fecha_fin, avance} = request.body;
-    const task = await insert({ tarea, descripcion_mensaje, apellido_materno, afecha_inicio, fecha_fin, avance});
+exports.crearTarea = async function (request, response) {
+    const {descripcion_mensaje, fecha_inicio, fecha_fin, avance} = request.body;
+    const task = await insert({ descripcion_mensaje, fecha_inicio, fecha_fin, avance});
     response.status(201).json(task);
 };
+
